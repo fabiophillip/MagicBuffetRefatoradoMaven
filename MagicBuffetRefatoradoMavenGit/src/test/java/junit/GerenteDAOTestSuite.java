156b9427dc80347ec58b5052b1621d1d29995cde
fabiophillip <haru_glory6@hotmail.com>
2013-11-14 21:14:36 -0300

    comitei no Eclipse GerenteDaoTestSuite e FestaDAOTestSuite

---
+++ MagicBuffetRefatoradoMavenGit/src/test/java/junit/GerenteDAOTestSuite.java
@@ -0,0 +1,70 @@
+package junit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import controler.Gerente;
+import entidadesDAO.GerenteDAO;
+
+public class GerenteDAOTestSuite {
+
+	private GerenteDAO gerenteDao;
+	@Before
+	public void setUp() throws Exception {
+		gerenteDao = new GerenteDAO();
+		Gerente gerenteLimparBd = new Gerente();
+		gerenteLimparBd.setLogin("fulanoQualquer");
+		gerenteLimparBd.setSenha("123667");
+		gerenteDao.remover(gerenteLimparBd);//se ja existir esse gerente...
+		
+		gerenteDao = new GerenteDAO();
+		Gerente gerenteParaBuscarBd = new Gerente();
+		gerenteParaBuscarBd.setLogin("fulano2");
+		gerenteParaBuscarBd.setSenha("127599");
+		gerenteDao.remover(gerenteParaBuscarBd);//se ja existir esse gerente...
+		gerenteDao.criar(gerenteParaBuscarBd);
+		
+		gerenteDao = new GerenteDAO();
+		Gerente gerenteParaRemover = new Gerente();
+		gerenteParaRemover.setLogin("fulano3");
+		gerenteParaRemover.setSenha("127596808");
+		gerenteDao.remover(gerenteParaRemover);//se ja existir esse gerente...
+		gerenteDao.criar(gerenteParaRemover);
+		
+	}
+
+	@Test
+	public void testCriarUmGerenteHappyPath() 
+	{
+		Gerente novoGerente = new Gerente();
+		novoGerente.setLogin("fulanoQualquer");
+		novoGerente.setSenha("123667");
+		boolean criouGerente = gerenteDao.criar(novoGerente);
+		assertEquals(criouGerente, true);
+	}
+	
+	@Test
+	public void testBuscarUmGerenteHappyPath()
+	{
+		
+		Gerente gerenteAchado = (Gerente) gerenteDao.buscar("fulano2");
+		assertEquals(gerenteAchado.getLogin(), "fulano2");
+		assertEquals(gerenteAchado.getSenha(), "127599");
+	}
+	
+	@Test
+	public void testRemoverUmGErenteHappyPath()
+	{
+		
+		Gerente gerenteParaRemover = new Gerente();
+		gerenteParaRemover.setLogin("fulano3");
+		gerenteParaRemover.setSenha("127596808");
+		boolean removeu = gerenteDao.remover(gerenteParaRemover);
+		assertEquals(true, removeu);
+	}
+	
+	
+
+}
