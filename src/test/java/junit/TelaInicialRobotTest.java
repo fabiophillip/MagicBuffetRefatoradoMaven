dddf6472e2affeec49c92aeb2a3eaccef78e43af
fabiophillip <haru_glory6@hotmail.com>
2013-11-19 14:44:54 -0200

    tirei teste inicial de RobotTest para ver se o construtor termina

--- src/test/java/junit/TelaInicialRobotTest.java
+++ src/test/java/junit/TelaInicialRobotTest.java
@@ -28,30 +28,7 @@ public class TelaInicialRobotTest {
 
 	@Test
 	public void test() {
-		/* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-		Robot robot;
-		try {
-			robot = new Robot();
-			int posicaoXBotaoBuscar = TelaInicial.getInstance().getBotaoBuscar().getLocation().x;
-			int posicaoYBotaoBuscar = TelaInicial.getInstance().getBotaoBuscar().getLocation().y;
-			
-			
-
-			robot.mouseMove(posicaoXBotaoBuscar, posicaoYBotaoBuscar);
-			robot.delay(3000);
-			
-			
-			robot.mousePress(InputEvent.BUTTON1_MASK);
-			robot.mouseRelease(InputEvent.BUTTON1_MASK);
-		} catch (AWTException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			fail("awt exception");
-		}
+	
 		
        
 	}
