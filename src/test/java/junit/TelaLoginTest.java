a9131991963632c6a87e1c8387cb85769145dd76
fabiophillip <haru_glory6@hotmail.com>
2013-11-20 10:39:22 -0200

    mudei TelaLoginTest cobertura com delay botão maior

--- src/test/java/junit/TelaLoginTest.java
+++ src/test/java/junit/TelaLoginTest.java
@@ -6,19 +6,26 @@ import java.awt.AWTException;
 import java.awt.Point;
 import java.awt.Robot;
 import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
 
 import javax.swing.JButton;
 
 import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 
+import controler.Gerente;
+import entidadesDAO.GerenteDAO;
 import view.telaLogin;
+
 
 public class TelaLoginTest {
 
-	telaLogin telaLogin;
+	static telaLogin telaLogin;
 	@Before
-	public void setUp() throws Exception {
+	public  void setUp() throws Exception {
 		telaLogin = new telaLogin();
 		view.telaLogin.main(null);
 		java.awt.EventQueue.invokeLater(new Runnable() {
@@ -43,16 +50,64 @@ public class TelaLoginTest {
 	        r.mousePress(InputEvent.BUTTON1_MASK);
 	        try { Thread.sleep(3000); } catch (Exception e) {}
 	        r.mouseRelease(InputEvent.BUTTON1_MASK);
-	        try { Thread.sleep(3000); } catch (Exception e) {}
+	        java.awt.EventQueue.invokeLater(new Runnable() {
+	            public void run() {
+	            	 try { Thread.sleep(3000); } catch (Exception e) {}
+	                
+	            }
+	        });
 		}
 		catch(AWTException exc)
 		{
 			exc.printStackTrace();
             fail("awt exception no teste da telaLogin clicar botao entrar");
 		}
-		
-       
+
+	}
+	
+	@Test
+	public void testEntrarComLoginInexistente()
+	{
+		try { Thread.sleep(3000); } catch (Exception e) {}
+		telaLogin.EntrarActionPerformed(null);
+		try { Thread.sleep(3000); } catch (Exception e) {}
 		
 	}
+	
+	/*
+	@Test
+	public void testZEntrarComLoginSenhaErrada()
+	{
+		Gerente gerenteCriar = new Gerente("admin", "admin");
+		GerenteDAO gerenteDAO = new GerenteDAO();
+		gerenteDAO.remover(gerenteCriar);
+		gerenteDAO.criar(gerenteCriar);
+		try { Thread.sleep(3000); } catch (Exception e) {}
+		telaLogin.getLoginTexto().setText("admin");
+		try { Thread.sleep(3000); } catch (Exception e) {}
+		telaLogin.EntrarActionPerformed(null);
+		try { Thread.sleep(3000); } catch (Exception e) {}
+		try {
+			Robot r = new Robot();
+			try { Thread.sleep(3000); } catch (Exception e) {}
+			 r.mousePress(InputEvent.BUTTON1_MASK);
+			 try { Thread.sleep(3000); } catch (Exception e) {}
+			try { Thread.sleep(3000); } catch (Exception e) {}
+			r.keyPress(KeyEvent.VK_ENTER);
+			
+			try { Thread.sleep(3000); } catch (Exception e) {}
+			
+		} catch (AWTException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			fail("test entrar coom login errado da view login está dando bug");
+		}
+		
+		
+		
+	}*/
+	
+	
 
 }
+
